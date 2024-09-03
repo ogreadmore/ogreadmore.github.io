33fefed298ce56d0ede706dca339e4e446674be6
ogreadmore <tayloroliphant@gmail.com>
2024-09-02 21:06:36 -0400

    yaya

--- handler.php
+++ handler.php
@@ -1,28 +1,54 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-/*
-Tested working with PHP5.4 and above (including PHP 7 )
 
- */
-require_once './vendor/autoload.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-use FormGuide\Handlx\FormHandler;
+require 'php/PHPMailer-5.2.28/src/Exception.php';
+require 'php/PHPMailer-5.2.28/src/PHPMailer.php';
+require 'php/PHPMailer-5.2.28/src/SMTP.php';
 
+$mail = new PHPMailer(true);
+$mail_to_email = '***@gmail.com'; // your email
+$mail_to_name = 'Webmaster';
 
-$pp = new FormHandler(); 
+try {
 
-$validator = $pp->getValidator();
-$validator->fields(['Name','Email'])->areRequired()->maxLength(50);
-$validator->field('Email')->isEmail();
-$validator->field('Message')->maxLength(6000);
-
-
-$pp->requireReCaptcha();
-$pp->getReCaptcha()->initSecretKey('copy-your-secret-key-here');
+	$mail_from_name = isset( $_POST['name'] ) ? $_POST['name'] : '';
+	$mail_from_email = isset( $_POST['email'] ) ? $_POST['email'] : '';
+	$mail_message = isset( $_POST['message'] ) ? $_POST['message'] : '';
 
 
-$pp->sendEmailTo('your-email-here'); // ← Your email here
+	// Server settings
+	$mail->isSMTP(); // Send using SMTP
+	$mail->Host = 'smtp.***.com'; // Set the SMTP server to send through
+	$mail->SMTPAuth = true; // Enable SMTP authentication
+	$mail->Username = '***'; // SMTP username
+	$mail->Password = '***'; // SMTP password
+	$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+	$mail->Port = 465; // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-echo $pp->process($_POST);
+	$mail->setFrom($mail_to_email, $mail_to_name); // Your email
+	$mail->addAddress($mail_from_email, $mail_from_name); // Add a recipient
+
+	// for($ct=0; $ct<count($_FILES['file_attach']['tmp_name']); $ct++) {
+	// 	$mail->AddAttachment($_FILES['file_attach']['tmp_name'][$ct], $_FILES['file_attach']['name'][$ct]);
+	// }
+
+	// Content
+	$mail->isHTML(true); // Set email format to HTML
+
+	$mail->Subject = $mail_title;
+	$mail->Body = '
+		<strong>Name:</strong> ' . $mail_from_name . '<br>
+		<strong>Email:</strong> ' . $mail_from_email . '<br>
+		<strong>Message:</strong> ' . $mail_message;
+
+	$mail->Send();
+
+	echo 'Message has been sent';
+
+} catch (Exception $e) {
+
+	echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+
+}
